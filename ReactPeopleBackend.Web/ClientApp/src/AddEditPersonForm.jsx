0ffcf0ed78e26d291e7f47472e4fc5372ae24230
Sara Reiss <Sarareiss003@gmail.com>
2023-05-19 17:34:23 -0400

    resaved

--- ReactPeopleBackend.Web/ClientApp/src/AddEditPersonForm.jsx
+++ ReactPeopleBackend.Web/ClientApp/src/AddEditPersonForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function AddEditPersonForm({ firstName, lastName, age, onTextChange, onAddClick, isAdding ,isEditMode,onUpdateClick,onCancelClick}) {
-    if(!isEditMode){
+   
         return <div className="row p-5 rounded" style={{backgroundColor: '#E9ECEF'}}>
         <div className="col-md-3">
             <input value={firstName} onChange={onTextChange} name='firstName' type="text" className="form-control" placeholder="First Name" />
@@ -12,26 +12,15 @@ export default function AddEditPersonForm({ firstName, lastName, age, onTextChan
         <div className="col-md-3">
             <input value={age} onChange={onTextChange} name='age' type="text" className="form-control" placeholder="Age" />
         </div>
-        <div className="col-md-3">
+        {!isEditMode &&   <div className="col-md-3">
             <button disabled={isAdding || isNaN(age) || age === '' || firstName===''||lastName===''} onClick={onAddClick} className='btn btn-primary w-100'>Add</button>
-        </div>
-    </div>
-    }
-    
-        return <div className="row p-5 rounded" style={{backgroundColor: '#E9ECEF'}}>
-        <div className="col-md-3">
-            <input value={firstName} onChange={onTextChange} name='firstName' type="text" className="form-control" placeholder="First Name" />
-        </div>
-        <div className="col-md-3">
-            <input value={lastName} onChange={onTextChange} name='lastName' type="text" className="form-control" placeholder="Last Name" />
-        </div>
-        <div className="col-md-3">
-            <input value={age} onChange={onTextChange} name='age' type="text" className="form-control" placeholder="Age" />
-        </div>
-        <div className="col-md-3">
+        </div>}
+        {isEditMode &&  <div className="col-md-3">
         <button className="btn btn-warning w-100" onClick={onUpdateClick}>Update</button>
         <button className="btn btn-dark w-100 mt-2"onClick={onCancelClick}>Cancel</button>
-        </div>
+        </div>}
     </div>
     
+    
+     
 }
